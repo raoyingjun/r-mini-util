0e71079a10980b598727aa467faf363a0bac0ec2
Royin <raoyingjun@foxmail.com>
2023-12-01 21:11:30 +0800

    添加对发布到npm的支持和调整

--- readme.md
+++ readme.md
@@ -1,4 +1,31 @@
-# RUtil
+# r-mini-util
 
-### RUtil 介绍
-RUtil 是一个前端工具集合，包含了许多方法以供使用
+### 介绍
+r-mini-util 是一个前端工具集，封装了许多常用方法以供使用。
+使用它，降低开发成本，提升开发效率！它本身由 `Typescript` 编写，
+为开发提供了极佳的便利性！
+
+### 如何使用？
+使用 ES Module 方式：
+```javascript
+import {randomNum} from 'RMiniUtil'
+
+randomNum(5, 10)
+```
+or
+```javascript
+
+import * as rMiniUtil from 'RMiniUtil'
+
+rMiniUtil.randomNum(5, 10)
+```
+全局变量方式访问：
+```javascript
+// var rMiniUtil = {/* Source Code */}
+rMiniUtil.sst()
+```
+
+### 完整 Demo 项目 
+
+你可以访问同目录下的 usage-demo，一个基于 vite 的 demo。其中演示了导入和使用 r-mini-util 的方式。
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,29 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const mode = process.env.NODE_ENV
-console.log('mode----------', mode)
-const isProd = mode === 'production'
-module.exports = {
-    entry: './src/index.ts',
-    resolve: {
-        extensions: ['.ts', '.tsx', '...'],
+const path = require("path");
 
+module.exports = (env, args) => {
+  const { mode } = args;
+  const isProd = mode === "production";
+
+  return {
+    entry: "./src/index.ts",
+    resolve: {
+      extensions: [".ts", ".tsx", "..."],
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "index.bundle.js"
+      path: path.resolve(__dirname, 'dist'),
+      filename: "index.js",
+      library: {
+        name: 'rMiniUtil',
+        type: 'umd'
+      },
+      clean: true
     },
     module: {
-        rules: [
-            {test: /\.ts$/, loader: "ts-loader"}
-        ]
+      rules: [{ test: /\.ts$/, loader: "ts-loader" }],
     },
     devServer: {
-        port: 8088,
+      port: 8088,
     },
-    mode: 'development',
-    plugins: [new HtmlWebpackPlugin({ template: './index.html' })],
-}
+    mode,
+  };
+};
